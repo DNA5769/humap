2c38dc9cbaa03f3b9d59857a4d68a767172c767c
DNA5769 <dennisthomas2002@gmail.com>
2021-04-10 03:05:33 +0530

    Added CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const apiRoutes = require('./api/router');
 
@@ -10,6 +11,7 @@ app.get('/', (req, res) => {
 });
 
 // Middleware
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
