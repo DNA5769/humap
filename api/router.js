33ec0580c567970a1aaad3b6a3f1bc3886aca528
DNA5769 <dennisthomas2002@gmail.com>
2021-04-09 21:31:22 +0530

    Backend: Setup mongoose

--- api/router.js
+++ api/router.js
@@ -1,7 +1,14 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const router = express.Router();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'api.html'));
