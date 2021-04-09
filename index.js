f7fc7464690ec1ad2944127b38bba81c20d01ac0
DNA5769 <dennisthomas2002@gmail.com>
2021-04-09 21:25:06 +0530

    Backend: Created basic api router which serves api template

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+
+const apiRoutes = require('./api/router');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+// Middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', apiRoutes);
 
 app.listen(port, () => console.log(`[SERVER] Started`));
