4f1b60b135fed1769e6fda5313c12f49fbf4bb55
DNA5769 <dennisthomas2002@gmail.com>
2021-04-09 22:13:56 +0530

    Backend: Created POST route to /create-user

--- api/router.js
+++ api/router.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
+const validator = require("email-syntax-validator");
+const md5 = require('md5');
 require('dotenv').config();
+
+const User = require('./models/user');
 
 const router = express.Router();
 
@@ -12,6 +16,58 @@ mongoose.connect(process.env.MONGO_URI, {
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'api.html'));
+});
+
+router.post('/create-user', (req, res) => {
+    if ('name' in req.body && 'email' in req.body && 'password' in req.body)
+    {
+        if (req.body.name.trim() === '')
+        {
+            res.status(401).json({
+                error: "Please enter a valid username"
+            });
+        }
+        else if (req.body.email.trim() === '' || !validator.validate(req.body.email.trim()))
+        {
+            res.status(401).json({
+                error: "Please enter a valid email"
+            });
+        }
+        else if (req.body.password.trim() === '')
+        {
+            res.status(401).json({
+                error: "Please enter a password"
+            });
+        }
+        else
+        {
+            let user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                name: req.body.name.trim(),
+                email: req.body.email.trim(),
+                password: req.body.password.trim(),
+                avatar: `https://www.gravatar.com/avatar/${md5(req.body.email.trim())}`
+            });
+
+            user.save()
+            .then(result => {
+                res.status(200).json({
+                    message: "User added sucessfully"
+                });
+            })
+            .catch(err => {
+                res.status(401).json({
+                    error: err
+                });
+            });
+        }
+    }
+    else
+    {
+        res.status(401).json({
+            error: "Body should contain name, email and password!"
+        });
+    }
 });
 
 module.exports = router;
