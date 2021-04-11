b464c1d8969eeb91f2c6097c58edb4ab77156940
DNA5769 <dennisthomas2002@gmail.com>
2021-04-11 08:38:16 +0530

    Setup backend for dev

--- backend/index.js
+++ backend/index.js
@@ -8,23 +8,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/create-post', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/get-ppt', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'Team ECMAniacs PPT.pdf'));
-});
-
-app.get('/get-demo', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'demo.html'));
+    res.redirect('/api');
 });
 
 // Middleware
-// app.use(cors());
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
