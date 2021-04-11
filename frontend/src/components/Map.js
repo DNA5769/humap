5f455c5afe533c5d445b19400f3990550d502659
DNA5769 <dennisthomas2002@gmail.com>
2021-04-11 07:16:35 +0530

    Mapbox baka :(

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect, useContext } from 'react'
 import ReactMapGL, { Marker } from 'react-map-gl'
+import mapboxgl from "mapbox-gl"
 import StateContext from '../StateContext'
 import UpdateContext from '../UpdateContext'
-import 'mapbox-gl/dist/mapbox-gl.css'
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 function Map(props) {
   const appState = useContext(StateContext)
@@ -46,7 +49,7 @@ function Map(props) {
     <div>
       <ReactMapGL className='mt-1 mr-9 map' {...viewport} onDblClick={handleDblClick} doubleClickZoom={false} mapStyle={'mapbox://styles/dna5769/ckn4km6x31sob17nzox3gjzdz'} onViewportChange={viewport => setViewport(viewport)} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API_ACCESS_TOKEN}>
         <Marker latitude={appState.currLocation[1]} longitude={appState.currLocation[0]}>
-          <svg xmlns='http://www.w3.org/2000/svg' width='30' height='30' fill='currentColor' class='bi bi-cursor' viewBox='0 0 16 16'>
+          <svg xmlns='http://www.w3.org/2000/svg' width='30' height='30' fill='currentColor' className='bi bi-cursor' viewBox='0 0 16 16'>
             <path d='M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52 2.25 8.184z' />
           </svg>
         </Marker>
