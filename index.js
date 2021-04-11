9048b2c987909986eb38638492c12aaf9bdb335d
DNA5769 <dennisthomas2002@gmail.com>
2021-04-11 08:24:48 +0530

    Removed Kars

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/get-demo', (req, res) => {
 });
 
 // Middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
